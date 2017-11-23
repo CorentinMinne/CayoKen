dc7842db8638975af15a736797154350e56f453c
Corentin Minne <corentin.minne@epitech.eu>
2017-11-23 13:29:36 +0100

    Modif animation par le temps

--- CayoKen.py
+++ CayoKen.py
@@ -5,8 +5,9 @@ from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((640, 480), RESIZABLE) 
+fenetre = pygame.display.set_mode((640, 480), RESIZABLE)
 pygame.display.set_caption("Cayo Ken")
+clock = pygame.time.Clock()
 
 
 ciel = pygame.image.load("cuiel").convert_alpha()
@@ -32,13 +33,17 @@ fond.image = pygame.image.load("fond.png").convert_alpha()
 fond.rect = fond.image.get_rect()
 fond.mask = pygame.mask.from_surface(fond.image)
 
-groupe = pygame.sprite.Group(fond, sol, cote_l, cote_r)
+groupe = pygame.sprite.Group(fond, cote_l, sol, cote_r)
 
-done = False 
+done = False
 
 def load_images(path):
     images = []
+    files = []
     for file_name in os.listdir(path):
+        files.append(file_name)
+    files = sorted(files)
+    for file_name in files:
         image = pygame.image.load(path + os.sep + file_name).convert()
         images.append(image)
     return images
@@ -47,11 +52,11 @@ class anim(pygame.sprite.Sprite):
 
     def __init__(self, position, images):
         size = (32, 32)  # This should match the size of the images.
-        
- 
+
+
         self.test = pygame.sprite.Sprite()
         self.test.rect = pygame.Rect(position, size)
-        
+
         self.images = images
         self.images_right = images
         self.images_left = [pygame.transform.flip(image, True, False) for image in images]  # Flipping every image.
@@ -72,7 +77,7 @@ class anim(pygame.sprite.Sprite):
         self.animation_frames = 6
         self.current_frame = 0
 
-    def update_frame_dependent(self):
+    def update_frame_dependent(self, dt):
         """
         Updates the image of Sprite every 6 frame (approximately every 0.1 second if frame rate is 60).
         """
@@ -83,24 +88,24 @@ class anim(pygame.sprite.Sprite):
             self.images = self.images_left
             self.image_jump = self.image_jump_l
 
-        self.current_frame += 1
-        if self.vx == 0 and self.vy == 0 and self.current_frame >= self.animation_frames:
-            self.current_frame = 0
+        self.current_time += dt
+        if self.vx == 0 and self.vy == 0 and self.current_time >= self.animation_time:
+            self.current_time = 0
             self.index = 0
             self.test.image = self.images[self.index]
-        elif self.vy != 0 and self.current_frame >= self.animation_frames:
-            self.current_frame = 0
+        elif self.vy != 0 and self.current_time >= self.animation_time:
+            self.current_time = 0
             self.index = 0
             self.test.image = self.image_jump
-        elif self.current_frame >= self.animation_frames:
-            self.current_frame = 0
+        elif self.current_time >= self.animation_time:
+            self.current_time = 0
             self.index = (self.index + 1) % len(self.images)
             self.test.image = self.images[self.index]
-        
-         #self.test.rect = self.test.rect.move(self.vx, 0)
+
+        #self.test.rect = self.test.rect.move(self.vx, 0)
 
     def update(self, dt):
-        self.update_frame_dependent()
+        self.update_frame_dependent(dt)
 
 images = load_images(path='temp')  # Make sure to provide the relative or full path to the images directory.
 player = anim(position=(640/2, 640/2), images=images)
@@ -113,10 +118,11 @@ move_decor = 0
 
 while not done:
 
+    dt = clock.tick(120) / 1000.0
     for event in pygame.event.get():
         if event.type == QUIT:
             done = True
-        
+
         elif event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 move_decor = -2
@@ -130,21 +136,21 @@ while not done:
                 jump = True
                 want_jump = True
                 player.vy = 2.5
-        
+
         elif event.type == KEYUP:
             if event.key == K_RIGHT or event.key == K_LEFT:
                 player.vx = 0
                 move_decor = 0
                 ap_l = False
                 ap_r = False
-    
-    
+
+
     if pygame.sprite.collide_mask(cote_r, player.test):
         if player.vx < 0:
             move_decor = 0
     elif ap_l == True:
         move_decor = 2
-        
+
     if pygame.sprite.collide_mask(cote_l, player.test):
         if player.vx > 0:
             move_decor = 0
@@ -156,8 +162,8 @@ while not done:
             player.vy = -1
             falling = True
             jump = False
-    
-    if falling == True:        
+
+    if falling == True:
         player.velocityy -= 1.0 * 1/60
         player.velocityy += player.vy
         player.test.rect.y -= player.vy
@@ -170,30 +176,30 @@ while not done:
         if want_jump == False:
             player.vy = 0
         player.test.rect.y = player.test.rect.y
-        
+
     elif jump == False:
         player.vy -= 1.8 * 1/60
         player.velocityy += player.vy
         player.test.rect.y -= player.vy
-      
+
     if jump == True and falling == False: # SI LE JOUEUR EST EN TRAIN DE SAUTER
         player.vy -= 1.8 * 1/60
         player.velocityy += player.vy
         player.test.rect.y -= player.vy
         time += 1
-        
+
     if time > 5:
         want_jump = False
-    
+
     for decor in groupe.sprites():
         decor.rect = decor.rect.move(move_decor, 0)
-    player.update(60)
+    player.update(dt)
     fenetre.blit(ciel, (0,0))
     #fenetre.blit(sol.image, (0,0))
     #fenetre.blit(fond.image, (0,0))
     groupe.update()
-    groupe.draw(fenetre)
-    fenetre.blit(player.test.image, player.test.rect)    
+    #groupe.draw(fenetre)
+    fenetre.blit(player.test.image, player.test.rect)
     pygame.display.flip()
 
 pygame.quit()
